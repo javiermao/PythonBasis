b577664e86dac432cdbf2454438dbbb60f42894a
Javier Mauricio Antelis Ortíz <mauricio.antelis@tec.mx>
2021-09-09 22:12:32 -0500

    Adding data files

---
+++ MyModules/MyModule2.py
@@ -0,0 +1,23 @@
+
+
+# =============================================
+# IMPORT MODULES AND LIBRARIES REQUIRED FOR THIS CUSTOM-MADE MODULE TO WORK
+# =============================================
+import math
+
+
+
+# =============================================
+# FUNCTION TO CALCULATE A USER SPECIFIC MATHEMATICAL FUNCTION
+# =============================================
+def Funtion1(x):
+    fx = 2 + 3*x - 4*math.pow(x,2)
+    return fx
+
+
+# =============================================
+# FUNCTION TO CALCULATE A USER SPECIFIC MATHEMATICAL FUNCTION
+# =============================================
+def Funtion2(x):
+    fx = - (2 + 3*x - 4*math.pow(x,2))
+    return fx
